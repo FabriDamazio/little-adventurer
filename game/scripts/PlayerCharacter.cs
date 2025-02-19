d56df509342c98928c77b95abb0b4db545275e79
FabriDamazio <fabridamazio@gmail.com>
2025-02-19 10:56:22 -0300

    Player mesh rotates towards movement direction

--- game/scripts/PlayerCharacter.cs
+++ game/scripts/PlayerCharacter.cs
@@ -2,34 +2,49 @@ using Godot;
 
 public partial class PlayerCharacter : CharacterBody3D
 {
-	public const float Speed = 5.0f;
+    public const float Speed = 5.0f;
 
-	public override void _PhysicsProcess(double delta)
-	{
-		Vector3 velocity = Velocity;
+    private Node3D _visualNode;
 
-		// Add the gravity.
-		if (!IsOnFloor())
-		{
-			velocity += GetGravity() * (float)delta;
-		}
+    public override void _Ready()
+    {
+        _visualNode = GetNode<Node3D>("%VisualNode");
+    }
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("left", "right", "up", "down");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-		}
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector3 velocity = Velocity;
 
-		Velocity = velocity;
-		MoveAndSlide();
-	}
+        // Add the gravity.
+        if (!IsOnFloor())
+        {
+            velocity += GetGravity() * (float)delta;
+        }
+
+        // Get the input direction and handle the movement/deceleration.
+        // As good practice, you should replace UI actions with custom gameplay actions.
+        Vector2 inputDir = Input.GetVector("left", "right", "up", "down");
+        Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+        if (direction != Vector3.Zero)
+        {
+            velocity.X = direction.X * Speed;
+            velocity.Z = direction.Z * Speed;
+        }
+        else
+        {
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+            velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+        }
+
+        Velocity = velocity;
+
+        if (Velocity.Length() > 0.2)
+        {
+            var lookDir = new Vector2(Velocity.Z, Velocity.X);
+            _visualNode.Rotation =
+              new Vector3(_visualNode.Rotation.X, lookDir.Angle(), _visualNode.Rotation.Z);
+        }
+
+        MoveAndSlide();
+    }
 }
