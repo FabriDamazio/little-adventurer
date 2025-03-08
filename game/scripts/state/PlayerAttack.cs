b648af7b069824d4d895675b183c6d6c6b7553d0
FabriDamazio <fabridamazio@gmail.com>
2025-03-08 09:15:31 -0300

    Player sword blade VFX

--- game/scripts/state/PlayerAttack.cs
+++ game/scripts/state/PlayerAttack.cs
@@ -5,14 +5,22 @@ public partial class PlayerAttack : StateBase
     [Export]
     public CollisionShape3D HitBoxCollisionShape;
 
+    [Export]
+    public AnimationPlayer BladeMaterialEffectAnimationPlayer;
+
+    [Export]
+    public Node3D VFXBlade;
+
     public void EnableHitBox()
     {
         HitBoxCollisionShape.Disabled = false;
+        GD.Print("hitbox enabled");
     }
 
     public void DisableHitBox()
     {
         HitBoxCollisionShape.Disabled = true;
+        GD.Print("hitbox disabled");
     }
 
     public override void Enter()
@@ -20,6 +28,10 @@ public partial class PlayerAttack : StateBase
         base.Enter();
 
         CharacterBody3D.Velocity = new Vector3(0, CharacterBody3D.Velocity.Y, 0);
+
+        VFXBlade.Visible = true;
+        BladeMaterialEffectAnimationPlayer.Stop();
+        BladeMaterialEffectAnimationPlayer.Play("PlayBladeVFX");
     }
 
     public override void Exit()
@@ -27,6 +39,7 @@ public partial class PlayerAttack : StateBase
         base.Exit();
 
         DisableHitBox();
+        VFXBlade.Visible = false;
     }
 
     public override void StateUpdate(double delta)
