74e0c4674ad88be66d0fb057e51f2d896deb31fb
FabriDamazio <fabridamazio@gmail.com>
2025-03-10 18:47:00 -0300

    Enemy dead material effect

--- game/scripts/EnemyDead.cs
+++ game/scripts/EnemyDead.cs
@@ -5,11 +5,17 @@ public partial class EnemyDead : EnemyStateBase
     [Export]
     public CollisionShape3D CollisionShape3D;
 
+    [Export]
+    public AnimationPlayer MaterialEffectAnimationPlayer;
+
     public override async void Enter()
     {
         base.Enter();
         CharacterBody3D.Velocity = Vector3.Zero;
         CollisionShape3D.Disabled = true;
+
+        await ToSignal(GetTree().CreateTimer(1), "timeout");
+        MaterialEffectAnimationPlayer.Play("dead");
 
         await ToSignal(GetTree().CreateTimer(3), "timeout");
 
