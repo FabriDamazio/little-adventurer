b7a69f720a06f190a754bd545c503f6a00f01017
FabriDamazio <fabridamazio@gmail.com>
2025-03-12 11:06:50 -0300

    Gate scene

---
+++ game/scripts/Gate.cs
@@ -0,0 +1,12 @@
+using Godot;
+
+public partial class Gate : Node3D
+{
+    [Export]
+    public AnimationPlayer AnimationPlayer;
+
+    public void Open()
+    {
+        AnimationPlayer.Play("Open");
+    }
+}
